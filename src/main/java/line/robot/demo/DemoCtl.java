05efdc63730058c00998875864bb91368a6c5ac2
alexander <catalyst0955@gmail.com>
2018-06-09 08:08:36 +0800

    new post method

--- src/main/java/line/robot/demo/DemoCtl.java
+++ src/main/java/line/robot/demo/DemoCtl.java
@@ -2,18 +2,30 @@ package line.robot.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
 public class DemoCtl {
 
     @RequestMapping("/init")
-    public String test(){
+    public String test() {
         return "index";
     }
 
     @RequestMapping("/start")
-    public String test2(){
+    public String test2() {
         return "foldeer/index2";
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    @ResponseBody
+    public String receivePostAction(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_OK);
+        return "OK";
     }
 }
