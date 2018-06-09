9f0d76b5c8b3b19bcf8e8b5dd15896651e2e21ef
alexander <catalyst0955@gmail.com>
2018-06-09 23:12:09 +0800

    測試建立Map資源做回圖機器人

--- src/main/java/line/robot/demo/LineBotService.java
+++ src/main/java/line/robot/demo/LineBotService.java
@@ -1,17 +1,31 @@
 package line.robot.demo;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@Service
+@Component
 public class LineBotService {
-     Map<String,String> valueMap;
+    Map<String, String> valueMap;
 
-    public void setValue(Map<String,String>valueMap){
-        this.valueMap = valueMap;
+    LineBotService() {
+        Map<String, String> picMap = new HashMap<>();
+        picMap.put("南非召喚", "https://i.imgur.com/BsOco1k.gifv");
+        picMap.put("羈押", "https://i.imgur.com/ac0BUWS.jpg");
+        picMap.put("錢包君1", "https://i.imgur.com/WtNrmCi.jpg");
+        picMap.put("賣萌", "https://i.imgur.com/x17YqKi.jpg");
+        picMap.put("裝傻", "https://i.imgur.com/cy1Gdq0.jpg");
+        picMap.put("什麼都沒有", "https://i.imgur.com/mCRGPgW.jpg");
+        picMap.put("萌新三連", "https://i.imgur.com/SAPBVzy.jpg");
+        picMap.put("萌新", "https://i.imgur.com/XiPQKCR.jpg");
+        this.valueMap = picMap;
+
+
     }
-    public String getPic(String key){
+
+
+    public String getPic(String key) {
         return valueMap.get(key);
     }
 
