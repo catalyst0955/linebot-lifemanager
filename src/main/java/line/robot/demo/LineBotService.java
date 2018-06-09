e60fe35325ad7f0ddbd1557826d2e9664ec2c6fd
alexander <catalyst0955@gmail.com>
2018-06-10 02:09:29 +0800

    測試Property

--- src/main/java/line/robot/demo/LineBotService.java
+++ src/main/java/line/robot/demo/LineBotService.java
@@ -31,7 +31,7 @@ public class LineBotService {
 
         Properties prop = new Properties();
         try{
-            prop.load(new FileInputStream( new ClassPathResource("/property/img.properties").getFile()));
+            prop.load(new FileInputStream( path+"/prop/img.properties"));
         } catch (Exception e) {
             e.printStackTrace();
         }
