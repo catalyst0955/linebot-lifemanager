15ac91d7a3ebb2c68bcd58284970f7b0974ca210
alexander <catalyst0955@gmail.com>
2018-06-10 12:18:32 +0800

    測試Property

--- src/main/java/line/robot/deadpool/DeadPoolCtl.java
+++ src/main/java/line/robot/deadpool/DeadPoolCtl.java
@@ -6,6 +6,7 @@ import com.linecorp.bot.model.message.ImageMessage;
 import line.robot.demo.DemoApplication;
 import line.robot.demo.LineBotCtl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.awt.*;
@@ -13,6 +14,7 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Component
 public class DeadPoolCtl {
 
     @Autowired
