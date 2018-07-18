7afb8938e20921ced00592039ecac5db380e77d5
alexander <catalyst0955@gmail.com>
2018-07-18 23:38:48 +0800

    init

--- src/main/java/line/robot/ctrl/DemoCtl.java
+++ src/main/java/line/robot/ctrl/DemoCtl.java
@@ -1,18 +1,17 @@
 package line.robot.ctrl;
 
 import line.robot.config.DataRepository;
-import line.robot.model.LineBotModal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 
 @Controller
@@ -41,17 +40,17 @@ public class DemoCtl {
 
     }
 
-//    @RequestMapping(value = "/post", method = RequestMethod.POST)
-//    public ResponseEntity receivePostAction(HttpServletRequest request, HttpServletResponse response) {
-//
-//        Map<String, String[]> parameterMap = request.getParameterMap();
-//        for(String key : parameterMap.keySet()){
-//            String[] valueSet = parameterMap.get(key);
-//            System.out.println( "KEY : " + key + "   VALUES:" + valueSet);
-//        }
-//
-//        response.setStatus(HttpServletResponse.SC_OK);
-//
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public ResponseEntity receivePostAction(HttpServletRequest request, HttpServletResponse response) {
+
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for(String key : parameterMap.keySet()){
+            String[] valueSet = parameterMap.get(key);
+            System.out.println( "KEY : " + key + "   VALUES:" + valueSet);
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
