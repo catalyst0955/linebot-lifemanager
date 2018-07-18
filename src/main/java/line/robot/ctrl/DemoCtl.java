1ba1e29cf6e8ad83841ec293ef67492e30830f11
alexander <catalyst0955@gmail.com>
2018-07-18 23:45:21 +0800

    123

--- src/main/java/line/robot/ctrl/DemoCtl.java
+++ src/main/java/line/robot/ctrl/DemoCtl.java
@@ -40,17 +40,17 @@ public class DemoCtl {
 
     }
 
-    @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public ResponseEntity receivePostAction(HttpServletRequest request, HttpServletResponse response) {
-
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        for(String key : parameterMap.keySet()){
-            String[] valueSet = parameterMap.get(key);
-            System.out.println( "KEY : " + key + "   VALUES:" + valueSet);
-        }
-
-        response.setStatus(HttpServletResponse.SC_OK);
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
+//    @RequestMapping(value = "/post", method = RequestMethod.POST)
+//    public ResponseEntity receivePostAction(HttpServletRequest request, HttpServletResponse response) {
+//
+//        Map<String, String[]> parameterMap = request.getParameterMap();
+//        for(String key : parameterMap.keySet()){
+//            String[] valueSet = parameterMap.get(key);
+//            System.out.println( "KEY : " + key + "   VALUES:" + valueSet);
+//        }
+//
+//        response.setStatus(HttpServletResponse.SC_OK);
+//
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
 }
